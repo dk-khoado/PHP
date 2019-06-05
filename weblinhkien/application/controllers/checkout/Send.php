8baf7e162dcf85d6055a317b502da7e9eb932848
kakoi-3nhan4-16 <violent12330@gmail.com>
2019-06-05 20:19:02 +0700

    Update Send.php

--- weblinhkien/application/controllers/checkout/Send.php
+++ weblinhkien/application/controllers/checkout/Send.php
@@ -15,8 +15,11 @@ class Send extends CI_Controller{
         $city = $this->input->post('city');
         $quan_huyen = $this->input->post('quan_huyen');
         $numberPhone= $this->input->post('numberphone');
-        $this->Customer->UpdateInformation($address, $numberPhone, $email, $name, $city, $quan_huyen);
+        $this->Customer->UpdateInformation($_SESSION['id'],$address, $numberPhone, $email, $name, $city, $quan_huyen);
         $data_cart = $this->Cart->getAllByIDUser($_SESSION['id']);
         $this->Order->Insert($_SESSION['id'], $name, $address, $city, $quan_huyen, $data_cart);
+        //phần gửi mail cho khách hàng
+        
+
     }
 }
