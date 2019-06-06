2193828696693a31c0cfc6400cfad6dbc9fe1cda
kakoi-3nhan4-16 <violent12330@gmail.com>
2019-06-07 02:15:53 +0700

    update tói ngày 6/6

--- weblinhkien/application/controllers/Test.php
+++ weblinhkien/application/controllers/Test.php
@@ -2,6 +2,9 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 class Test extends CI_Controller{
     public function index(){
-        echo "123a"."12";
+        echo "<center>";
+        echo "<h1>Hello Word!!</h1>";
+        echo "</center>";
+        
     }
 }
