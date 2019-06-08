78961690635eb90b39a9f245171f3bad089506dd
haivl4nhan3bang16 <duy.haivl321@gmail.com>
2019-06-08 18:45:37 +0700

    LoadCart

--- weblinhkien/application/views/cart.php
+++ weblinhkien/application/views/cart.php
@@ -1,3 +1,8 @@
+<div class="row">
+	<div class="col-4"><button type="button" class="btn btn-danger btn-sm" style="width: 80%; text-align: center;"><strong>Tiếp tục mua hàng</strong></button></div>
+	<div class="col-4">Tổng tiền 1000000</div>
+	<div class="col-4"><button type="button" class="btn btn-danger btn-sm" style="width: 80%; text-align: center;"><strong>Thanh toán</strong></button></div>
+</div>
 
 <div class="row">
 	<div class="col-1"><img src="#" width="80px" height="80px"></div>
@@ -9,14 +14,18 @@
 			<div class="col-2">TTiền</div>
 			<div class="col-1"></div>
 		</div>
-		
-		<div class="row">
-			<div class="col-4">Hàivl</div>
-			<div class="col-4">99999 đ</div>
-			<div class="col-1"><input class="form-control text-center" min="1" max="50" type="number" style="width: 60px; height: 25px;"></input></div>
-			<div class="col-2">65765766 đ</div>
-			<div class="col-1"><button type="button" class="btn btn-danger btn-sm" style="width: 30px; height: 30px; text-align: center;"><strong>X</strong></button></div>
-		</div>
+		<?php
+		foreach($data as $key => $value)
+		{
+				echo '<div class="row">';
+				echo '<div class="col-4">'.$value->NameProduct.'</div>';
+				echo '<div class="col-4">'.$value->PriceProduct.'</div>';
+				echo '<div class="col-1"><input class="form-control text-center" value='.$value->amount.' min="1" max="50" type="number" style="width: 60px; height: 25px;"></div>';
+				echo '<div class="col-2">ko biết hihi</div>';
+				echo '<div class="col-1"><button type="button" class="btn btn-danger btn-sm" style="width: 30px; height: 30px; text-align: center;"><strong>X</strong></button></div>';
+				echo '</div>';
+		}
+		?>
 	</div>
 
 </div>
