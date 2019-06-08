16a05a54d758a2ddbd17b50e63dcc19075eb2d5a
haivl4nhan3bang16 <duy.haivl321@gmail.com>
2019-06-08 22:45:41 +0700

    Quản lý khu vựa

--- weblinhkien/application/controllers/ApiAjax.php
+++ weblinhkien/application/controllers/ApiAjax.php
@@ -8,7 +8,8 @@ class ApiAjax  extends CI_Controller
         parent::__construct();
         $this->load->model('Customer');
         $this->load->model('Products');
-        $this->load->model('Cart');       
+        $this->load->model('Cart');
+		$this->load->model('Country');
     }
     public function checkLogin()
     {
@@ -120,18 +121,59 @@ class ApiAjax  extends CI_Controller
         $data = $this->Order->getDetail($idorder);
         echo json_encode($data);
     }
+	//Từ đây xuống dưới là của Duy nhá
+	//thành công
 	public function loadDistrist()
 	{
         $this->load->model("Country");
 		$distrist = $this->input->post("ID_city");
 		$data = $this->Country->Distrist($distrist);
 		echo json_encode($data);
-    }
-    public function resetPass(){
-        $email= $this->input->post("email");       
-        $this->load->model("SendMail");
-        $this->Customer->ResetPassword($email);
-        $context =$this->load->view("form_mail/sendResetPass",array("email"=>$email), true);;
-        $this->SendMail->sendResetPassword($email, $context);    
-    }
+	}
+	//thành công
+	public function insertcity()
+	{
+		$this->load->model("Country");
+		$city = $this->input->post("city_name");
+		$data = $this->Country->Insert($city);
+	}
+	//thành công
+	public function loadcity()
+	{
+		$this->load->model("Country");
+		$data = $this->Country->Province();
+		echo json_encode($data);
+	}
+	//thàng công
+	public function removeCity()
+	{
+		$ID_city = $this->input->post("ID_city");
+        $this->Country->DeleteByID($ID_city);
+	}
+	//thành công
+	public function AddDistrist()
+	{
+		$this->load->model("Country");
+		$id_city = $this->input->post("ID_city");
+		$quan_huyen = $this->input->post("quan_huyen_name");
+		$data = $this->Country->InsertDis($id_city, $quan_huyen);
+		
+	}
+	//thành công
+	public function UpdateCity()
+	{
+		$this->load->model("Country");
+		$city_name = $this->input->post("city_name");
+		$ID_city = $this->input->post("ID_city");
+		$this->Country->UpdateCity($city_name, $ID_city);
+		
+	}
+	
+//	public function LoadCityToModal()
+//	{
+//		$this->load->model("Country");
+//		$city = $this->input->post("ID_city");
+//		$data = $this->Country->LoadCityToModal($city);
+//		echo json_encode($data);
+//	}
 }
